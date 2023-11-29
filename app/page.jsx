75cb53f3e8fe6beb8f1a9081e4a3e2c3a5590b24
Conor <conordunne@outlook.ie>
2023-11-29 01:10:07 +0000

    Update css

--- app/page.jsx
+++ app/page.jsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 
 export default function App() {
   return (
-    <>
+    <div className="container px-6">
       <h1>Hello</h1>
-      <ul>
+      <ul className="list-disc">
         <li>
           <Link href="/tinymce">Tinymce</Link>
         </li>
       </ul>
-    </>
+    </div>
   );
 }
